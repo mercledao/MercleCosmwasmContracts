faa858e200ce257444153f56d5857ac152210968
divyesh87 <divyeshlalwani13@gmail.com>
2024-07-26 12:53:36 +0530

    Client side script

--- node.sh
+++ node.sh
@@ -15,46 +15,46 @@ else
 fi
 
 
-xiond init $MONIKER --chain-id $CHAIN_ID --home $HOME
+xiond init $MONIKER --chain-id $CHAIN_ID --home $HOME --default-denom uxion
 
 npm run gen-key --silent > keys/validator.key
 VAL_KEY=$(cat keys/validator.key)
 echo $VAL_KEY | xiond keys add validator1 --keyring-backend test --home $HOME --recover 
 MY_VALIDATOR_ADDRESS=$(xiond keys show validator1 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_VALIDATOR_ADDRESS 10000000stake --home $HOME
+xiond genesis add-genesis-account $MY_VALIDATOR_ADDRESS 10000000uxion --home $HOME
 
 npm run gen-key --silent > keys/account1.key
 ACC1_KEY=$(cat keys/account1.key)
 echo $ACC1_KEY | xiond keys add account1  --keyring-backend test --home $HOME --recover 
 MY_ACC1_ADDRESS=$(xiond keys show account1 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_ACC1_ADDRESS 1000000000stake --home $HOME
+xiond genesis add-genesis-account $MY_ACC1_ADDRESS 1000000000uxion --home $HOME
 
-npm run gen-key --silent > keys/account2.key
-ACC2_KEY=$(cat keys/account2.key)
-echo $ACC2_KEY | xiond keys add account2  --keyring-backend test --home $HOME --recover 
-MY_ACC2_ADDRESS=$(xiond keys show account2 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_ACC2_ADDRESS 1000000000stake --home $HOME
+# npm run gen-key --silent > keys/account2.key
+# ACC2_KEY=$(cat keys/account2.key)
+# echo $ACC2_KEY | xiond keys add account2  --keyring-backend test --home $HOME --recover 
+# MY_ACC2_ADDRESS=$(xiond keys show account2 -a --keyring-backend test --home $HOME)
+# xiond genesis add-genesis-account $MY_ACC2_ADDRESS 1000000000stake --home $HOME
 
-npm run gen-key --silent > keys/account3.key
-ACC3_KEY=$(cat keys/account3.key)
-echo $ACC3_KEY | xiond keys add account3  --keyring-backend test --home $HOME --recover 
-MY_ACC3_ADDRESS=$(xiond keys show account3 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_ACC3_ADDRESS 1000000000stake --home $HOME
+# npm run gen-key --silent > keys/account3.key
+# ACC3_KEY=$(cat keys/account3.key)
+# echo $ACC3_KEY | xiond keys add account3  --keyring-backend test --home $HOME --recover 
+# MY_ACC3_ADDRESS=$(xiond keys show account3 -a --keyring-backend test --home $HOME)
+# xiond genesis add-genesis-account $MY_ACC3_ADDRESS 1000000000stake --home $HOME
 
-npm run gen-key --silent > keys/account4.key
-ACC4_KEY=$(cat keys/account4.key)
-echo $ACC4_KEY | xiond keys add account4  --keyring-backend test --home $HOME --recover 
-MY_ACC4_ADDRESS=$(xiond keys show account4 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_ACC4_ADDRESS 1000000000stake --home $HOME
+# npm run gen-key --silent > keys/account4.key
+# ACC4_KEY=$(cat keys/account4.key)
+# echo $ACC4_KEY | xiond keys add account4  --keyring-backend test --home $HOME --recover 
+# MY_ACC4_ADDRESS=$(xiond keys show account4 -a --keyring-backend test --home $HOME)
+# xiond genesis add-genesis-account $MY_ACC4_ADDRESS 1000000000stake --home $HOME
 
-npm run gen-key --silent > keys/account5.key
-ACC5_KEY=$(cat keys/account5.key)
-echo $ACC5_KEY | xiond keys add account5  --keyring-backend test --home $HOME --recover 
-MY_ACC5_ADDRESS=$(xiond keys show account5 -a --keyring-backend test --home $HOME)
-xiond genesis add-genesis-account $MY_ACC5_ADDRESS 1000000000stake --home $HOME
+# npm run gen-key --silent > keys/account5.key
+# ACC5_KEY=$(cat keys/account5.key)
+# echo $ACC5_KEY | xiond keys add account5  --keyring-backend test --home $HOME --recover 
+# MY_ACC5_ADDRESS=$(xiond keys show account5 -a --keyring-backend test --home $HOME)
+# xiond genesis add-genesis-account $MY_ACC5_ADDRESS 1000000000stake --home $HOME
 
 # Create a gentx.
-xiond genesis gentx validator1 10000000stake --chain-id $CHAIN_ID --keyring-backend test --home $HOME
+xiond genesis gentx validator1 10000000uxion --chain-id $CHAIN_ID --keyring-backend test --home $HOME
 
 # Add the gentx to the genesis file.
 xiond genesis collect-gentxs --home $HOME
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,8 @@
-import { SigningStargateClient } from "@cosmjs/stargate";
-import { DirectSecp256k1HdWallet, OfflineDirectSigner } from "cosmwasm";
+import { Secp256k1HdWallet } from "@cosmjs/amino";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { Decimal } from "@cosmjs/math";
+import { expect } from "chai";
+import { readFileSync } from "fs";
 import { readFile } from "fs/promises";
 
 const rpc = "http://localhost:26657";
@@ -8,17 +11,60 @@ async function main() {
   const signer = await getAliceSignerFromMnemonic();
   const [acc] = await signer.getAccounts();
 
-  const client = await SigningStargateClient.connectWithSigner(rpc, signer);
-  console.log(await client.getAllBalances(acc.address));
+  const client = await SigningCosmWasmClient.connectWithSigner(rpc, signer, {
+    gasPrice: { amount: Decimal.fromUserInput("0.1", 3), denom: "uxion" },
+  });
+  const upload = await client.upload(
+    acc.address,
+    readFileSync("./contracts/artifacts/contract.wasm"),
+    "auto"
+  );
+
+  console.log("Instantiating contract.....");
+  const instance = await client.instantiate(
+    acc.address,
+    upload.codeId,
+    {
+      count: "100",
+    },
+    "test",
+    "auto"
+  );
+
+  const countRes = await client.queryContractSmart(instance.contractAddress, {
+    current: {},
+  });
+
+  console.log("Testing if correctly initialized");
+  expect(+countRes.message).equal(100);
+
+  await client.execute(
+    acc.address,
+    instance.contractAddress,
+    "increment",
+    "auto"
+  );
+
+  const countResAgain = await client.queryContractSmart(
+    instance.contractAddress,
+    {
+      current: {},
+    }
+  );
+
+  console.log("Expecting increment");
+  expect(+countResAgain.message).equal(101);
+
+  console.log("Deploy success");
 }
 
 main();
 
-async function getAliceSignerFromMnemonic(): Promise<OfflineDirectSigner> {
-  return DirectSecp256k1HdWallet.fromMnemonic(
+async function getAliceSignerFromMnemonic() {
+  return Secp256k1HdWallet.fromMnemonic(
     (await readFile("./keys/account1.key")).toString(),
     {
-      prefix: "wasm",
+      prefix: "xion",
     }
   );
 }
